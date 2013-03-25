b93108f1e3d7b57ef58ae6d5a0297259e08e548b
Przybylski 중광 <r351574nc3@gmail.com>
2013-03-24 17:21:22 -0700

    Using junit 4 test. Adding screenshots.

--- Chapter1/src/test/java/com/github/r351574nc3/packtpub/junit/AppTest.java
+++ Chapter1/src/test/java/com/github/r351574nc3/packtpub/junit/AppTest.java
@@ -1,38 +1,26 @@
 package com.github.r351574nc3.packtpub.junit;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.Ignore;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
 /**
- * Unit test for simple App.
+ * Tests for {@link Foo}.
+ *
+ * @author user@example.com (John Doe)
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+@RunWith(JUnit4.class)
+public class AppTest {
+
+    @Test
+    public void thisAlwaysPasses() {
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    @Test
+    @Ignore
+    public void thisIsIgnored() {
     }
 }
