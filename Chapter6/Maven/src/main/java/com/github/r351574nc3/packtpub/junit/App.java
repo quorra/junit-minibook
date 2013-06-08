4b8538e7a14808fbf3b0b80391adb189dc9ffae4
Przybylski 중광 <r351574nc3@gmail.com>
2013-06-08 11:08:28 -0700

    Configuring App for exceptions. Fixing Chapter 7 pom

--- Chapter6/Maven/src/main/java/com/github/r351574nc3/packtpub/junit/App.java
+++ Chapter6/Maven/src/main/java/com/github/r351574nc3/packtpub/junit/App.java
@@ -4,10 +4,13 @@ package com.github.r351574nc3.packtpub.junit;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public App() {
+    }
+
+    public App(final String param) {
+        if (param == null) {
+            throw new IllegalArgumentException();
+        }
     }
 }
--- Chapter6/Maven/src/test/java/com/github/r351574nc3/packtpub/junit/chapter6/AppTest.java
+++ Chapter6/Maven/src/test/java/com/github/r351574nc3/packtpub/junit/chapter6/AppTest.java
@@ -1,6 +1,36 @@
+/*
+ *  Copyright (c) 2013, Leo Przybylski
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met: 
+ *
+ *  1. Redistributions of source code must retain the above copyright notice, this
+ *  list of conditions and the following disclaimer. 
+ *  2. Redistributions in binary form must reproduce the above copyright notice,
+ *  this list of conditions and the following disclaimer in the documentation
+ *  and/or other materials provided with the distribution. 
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ *  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ *  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ *  ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ *  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ *  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ *  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ *  The views and conclusions contained in the software and documentation are those
+ *  of the authors and should not be interpreted as representing official policies, 
+ *  either expressed or implied, of the FreeBSD Project.
+ */
 package com.github.r351574nc3.packtpub.junit.chapter6;
 
 import static org.junit.Assert.assertEquals;
+
+import com.github.r351574nc3.packtpub.junit.App;
 
 import org.junit.Test;
 import org.junit.Ignore;
@@ -10,22 +40,19 @@ import org.junit.runners.JUnit4;
 /**
  * Tests for {@link Foo}.
  *
- * @author user@example.com (John Doe)
+ * @author Leo Przybylski
  */
 @RunWith(JUnit4.class)
 public class AppTest {
 
-    @Test
-    public void thisAlwaysPasses() {
+    @Test(expected = IllegalArgumentException.class)
+    public void thisFails() {
+        new App("Parameter");
     }
 
-    @Test
-    public void compareIntegers() {
-        assertEquals("failure - strings not same", 5l, 4l);
+    @Test(expected = IllegalArgumentException.class)
+    public void thisSucceeds() {
+        new App(null);
     }
 
-    @Test
-    @Ignore
-    public void thisIsIgnored() {
-    }
 }
