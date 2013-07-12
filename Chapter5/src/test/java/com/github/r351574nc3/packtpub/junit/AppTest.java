3702d112902b9516da748e6259a0223523be2509
Przybylski 중광 <r351574nc3@gmail.com>
2013-07-12 08:42:55 -0700

    Adding tests that pass

---
+++ Chapter5/src/test/java/com/github/r351574nc3/packtpub/junit/AppTest.java
@@ -0,0 +1,26 @@
+package com.github.r351574nc3.packtpub.junit;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.Ignore;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/**
+ * Tests for {@link Foo}.
+ *
+ * @author user@example.com (John Doe)
+ */
+@RunWith(JUnit4.class)
+public class AppTest {
+
+    @Test
+    public void thisAlwaysPasses() {
+    }
+
+    @Test
+    @Ignore
+    public void thisIsIgnored() {
+    }
+}
